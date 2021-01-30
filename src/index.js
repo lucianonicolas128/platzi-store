5e31bf116e90e513f3ffc5c11ce379e13a11129b
Luciano Nicolás <luciano.prina@hotmail.es>
2021-01-30 08:03:34 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Header from './components/Header';
+import App from './containers/App';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+
+    document.getElementById('app'));
